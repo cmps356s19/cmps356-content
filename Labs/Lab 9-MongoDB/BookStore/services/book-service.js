6e24fbf300b746704f5edd9f33cdab7c7bb61d22
abdalabaaji <abdala.baaji@gmail.com>
2019-03-29 23:12:01 +0300

    ...;

--- Labs/Lab 9-MongoDB/BookStore/services/book-service.js
+++ Labs/Lab 9-MongoDB/BookStore/services/book-service.js
@@ -37,15 +37,15 @@ class BookService {
             if (req.query.name)
                 books = await bookRepo.getBooksByName(req.query.name);
             else if (req.query.pageCount)
-                books =  bookRepo.getBooksByPageCount(parseInt(req.query.pageCount));
+                books =  await bookRepo.getBooksByPageCount(parseInt(req.query.pageCount));
             else if (req.query.author)
-                books =  bookRepo.getBooksByAuthor(req.query.author);
+                books =  await bookRepo.getBooksByAuthor(req.query.author);
             else if (req.query.category)
-                books =  bookRepo.getBooksByCategory(req.query.category);
+                books =  await bookRepo.getBooksByCategory(req.query.category);
             else if (req.query.isbn)
-                books =  bookRepo.getBookByISBN(req.query.isbn);
+                books =  await bookRepo.getBookByISBN(req.query.isbn);
             else
-                books =  bookRepo.getBooks();
+                books =  await bookRepo.getBooks();
 
             res.status(200).json(books);
         } catch (err) {
